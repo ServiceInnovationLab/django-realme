ab6ecb5df214828561df0c807c16dff0edcce8d9
Jonathan Harker <jonathan@catalyst.net.nz>
2018-01-31 14:56:04 +1300

    Tag a 1.0 release for PyPI.

--- setup.py
+++ setup.py
@@ -2,11 +2,20 @@ import os
 from os.path import abspath, dirname, join
 from setuptools import find_packages, setup
 
+
 HERE = abspath(dirname(__file__))
 os.chdir(HERE)  # allow setup.py to be run from any path
 
-with open(join(HERE, 'README.md')) as readme:
-    README = readme.read()
+try:
+    # Use pypandoc to convert Markdown to RST; Markdown is not supported on PyPI
+    import pypandoc
+    print("Converting README.md to RST")
+    README = pypandoc.convert(join(HERE, 'README.md'), 'rst')
+except(IOError, ImportError):
+    print("Markdown unsupported by PyPI, README.md will be plain-text.")
+    print("Recommend installing pypandoc.")
+    with open(join(HERE, 'README.md')) as readme:
+        README = readme.read()
 
 with open(join(HERE, 'requirements.txt')) as requirements:
     REQUIREMENTS = list(requirements.readlines())
@@ -18,9 +27,9 @@ setup(
     include_package_data=True,
     description='A Django app client for the RealMe service',
     long_description=README,
-    url='',
+    url='https://github.com/catalyst/django-realme',
     author='Catalyst IT',
-    author_email='',
+    author_email='pypi@catalyst.net.nz',
     license='GPL',
     classifiers=[
         'Environment :: Web Environment',
